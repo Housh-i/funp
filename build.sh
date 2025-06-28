36d8d39aa6330e79d142062f941674aa8232b895
Houshi-i <dimavelkk@gmail.com>
2025-06-29 00:43:39 +0300

    +

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python src/manage.py collectstatic --no-input
+python src/manage.py collectstatic --no-input --ignore css/main.css
 
 # Apply any outstanding database migrations
 python src/manage.py migrate
